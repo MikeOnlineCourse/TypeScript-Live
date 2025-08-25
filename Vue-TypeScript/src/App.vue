e169544cc2f78d901538c480fcfa6f63f9b0594d
mike <a3804430@hotmail.com>
2025-08-25 09:53:00 +0800

    fix: remove demo

--- Vue-TypeScript/src/App.vue
+++ Vue-TypeScript/src/App.vue
@@ -2,14 +2,19 @@
   // import ref from '@/components/01-ref.vue';
   // import reactive from '@/components/02-reactive.vue';
   // import computed from '@/components/03-computed.vue';
-  // import eventTarget from '@/components/04-EventTarget.vue';
-  import domRef from '@/components/05-DomRef.vue';
+  import eventTarget from '@/components/04-EventTarget.vue';
 </script>
 
 <template>
   <div>
-    <domRef />
+    <eventTarget />
   </div>
 </template>
 
-<style scoped></style>
+<style>
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+</style>
