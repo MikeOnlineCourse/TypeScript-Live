03a079f67780f4e40e0a69067576e7a5c8d7e0cb
MikeCheng1208 <a3804430@hotmail.com>
2025-10-11 11:39:44 +0800

    fix: demo

--- exercise/TypeScript-Base/src/ts/09-TypeAssertion.ts
+++ exercise/TypeScript-Base/src/ts/09-TypeAssertion.ts
@@ -1,16 +1,16 @@
 // unknown 斷言基礎型別
 const priceNum: unknown = 3.14159;
-const countNun: number = priceNum;
+const countNun = priceNum;
 console.log(countNun.toFixed());
 
 // 寫法二（不常見）
 const priceNum2: unknown = 3.14159;
-const countNum2: number = priceNum2;
+const countNum2 = priceNum2;
 console.log(countNum2.toFixed());
 
 // unknown 斷言基礎型別
 const strValue: unknown = "this is a string";
-const strLength: number = strValue.length;
+const strLength = strValue.length;
 console.log(strLength);
 
 // unknown 斷言物件型別
