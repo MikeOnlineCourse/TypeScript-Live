e343c30c8b108e0901c88dc8ee0316d519b8e162
mike <a3804430@hotmail.com>
2025-10-29 13:51:07 +0800

    fix: add new code

--- example/Vue-TypeScript/src/components/08-emit.vue
+++ example/Vue-TypeScript/src/components/08-emit.vue
@@ -1,4 +1,10 @@
 <script setup lang="ts">
+  // const emit = defineEmits<{
+  //   (e: 'activeIdx', id: number): void;
+  //   (e: 'updateName', name: string): void;
+  // }>();
+
+  // Vue 3.3+ 之後更簡單的寫法
   const emit = defineEmits<{
     activeIdx: [id: number];
     updateName: [name: string];
