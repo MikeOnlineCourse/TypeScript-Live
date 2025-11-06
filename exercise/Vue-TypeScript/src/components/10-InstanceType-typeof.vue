6568806016a6803d73b4870d57f1f68af939f654
MikeCheng1208 <a3804430@hotmail.com>
2025-11-06 19:21:27 +0800

    fix: exercise demo

---
+++ exercise/Vue-TypeScript/src/components/10-InstanceType-typeof.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+  import { ref } from 'vue';
+
+  const isOpen = ref(false);
+
+  const productTitle = ref('');
+
+  const open = () => {
+    isOpen.value = true;
+  };
+
+  const close = () => {
+    isOpen.value = false;
+  };
+
+  const setTitle = (title: string) => {
+    productTitle.value = title;
+  };
+
+  defineExpose({
+    open,
+    close,
+    setTitle,
+  });
+</script>
+
+<template>
+  <div v-if="isOpen" class="modal">
+    <h2>{{ productTitle }}</h2>
+    <slot />
+    <button @click="close">關閉</button>
+  </div>
+</template>
